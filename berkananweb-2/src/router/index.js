b7774cc4d98530428d75a5e99899819bf620bd9c
PeetaW <h7337473@hotmail.com.tw>
2025-02-13 14:06:20 +0800

    remove unnecessary router link

--- berkananweb-2/src/router/index.js
+++ berkananweb-2/src/router/index.js
@@ -1,24 +1,10 @@
 // router.js
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import PeterDesign from '../views/PeterDesing.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/peterdesign',
     name: 'peterdesign',
     component: PeterDesign
   }
